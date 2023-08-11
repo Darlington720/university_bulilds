28b11ac6c979348d7c62f422bed0e200b15f1567
Darlington720 <darlingtonakampa@gmail.com>
2023-08-12 00:55:22 +0300

    testing dashboard speed

---
+++ dist/assets/ExamsTimetable.c7e653f0.js
@@ -0,0 +1,1 @@
+import{b as e}from"./vendor.d738f66a.js";function t(){return e("div",{children:"ExamsTimetable"})}export{t as default};
---
+++ dist/assets/Malpractice.c0eca6d2.js
@@ -0,0 +1,1 @@
+import{b as r}from"./vendor.d738f66a.js";function t(){return r("div",{children:"Malpractice"})}export{t as default};
