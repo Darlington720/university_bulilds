b1b896974b0a28df0e002ee1afb6d199add06ec1
Darlington720 <darlingtonakampa@gmail.com>
2023-08-27 18:42:54 +0300

    added timetable module

---
+++ dist/assets/ExamsTimetable.46e0c48d.js
@@ -0,0 +1,1 @@
+import{b as e}from"./vendor.9c0f602b.js";function t(){return e("div",{children:"ExamsTimetable"})}export{t as default};
---
+++ dist/assets/Malpractice.0d25d520.js
@@ -0,0 +1,1 @@
+import{b as r}from"./vendor.9c0f602b.js";function t(){return r("div",{children:"Malpractice"})}export{t as default};
