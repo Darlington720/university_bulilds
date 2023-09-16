5be7eba8b25e655922058ab21c6743335e124621
Darlington720 <darlingtonakampa@gmail.com>
2023-09-16 02:04:55 -0400

    updated contents in user accounts

--- nkumbaServer.js
+++ nkumbaServer.js
@@ -23,7 +23,7 @@ const secret = "mySecret";
 app.use(express.json({ limit: "10mb" }));
 app.use(cors());
 app.use(express.static(__dirname + "/public"));
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 app.use(fileUpload());
 app.use(
   bodyParser.urlencoded({
@@ -116,7 +116,7 @@ const data = [
 ];
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.post("/storeCookie/", (req, res) => {
